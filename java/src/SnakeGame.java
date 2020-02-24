a728784d509d350bbfa793182c1dbbc35d57d056
Valeria <vggonzalezm@miners.utep.edu>
2020-02-24 14:47:52 -0700

    Attendance

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -19,32 +19,75 @@ public class SnakeGame
 
     public int[]findTailExhaustive(boolean [][] board)
     {
+
+        // RESET THE COUNTER
+        exhaustiveCheck = 0;
+
         // need to look at the current position (0,0) then neighbors if it is part of the snake,if it is not move to the next cell
         // not to be the head (1 neighbor)
         // if it has 2 or more move the next cell
         // return tail (x,y)
         // counter to count the true cell
-        int [] tailPosition;
+        int [] tailPosition = new int [3];
         int tailLength = 0;
-        for(int i = 0; i < board.length; i++)
+        for(int row = 0; row < board.length; row++)
         {
-            for(int j = 0; j< board[i].length; j++)
+            for(int col = 0; col< board[row].length; col++)
             {
-                int hold = neighbors(i,j);
-                if(board [i][j] == true)
+                // INCREMENT EXHAUSTIVE CHECKS AND REST WHEN FOUND IT
+                exhaustiveCheck++;
+                int hold = neighbors(row,col);
+                if(board [row][col] == true)
                 {
-                    tailLength++;
-                    if (hold == 1 && i != headPosition[0] && j!= headPosition[1])
+                    tailPosition[2]++;
+                    if (hold == 1 && row != headPosition[0] && col!= headPosition[1])
                     {
-                        tailPosition = new int [i, j, tailLength];
-                        return tailPosition; // return and array of length [3] 1) tail position -- x,y 3) tailLength
+
+                        tailPosition[0]= row;
+                        tailPosition[1] = col;
+                        exhaustiveCheck--;
+
                     }
                 }
             }
         }
 
+        return tailPosition; // return and array of length [3] 1) tail position -- x,y 3) tailLength
+
     }
 
+
+    public int[] findTailRecursive()
+    {
+
+        // RESET THE COUNTER
+        recursiveChecks= 0;
+
+        // Find the Head position
+
+        for (int row = 0; row < game.length; row++) // rows
+        {
+            for (int col = 0; col < game[row].length; col++) // columns
+            {
+                if (row == headPosition[0] && col == headPosition[1])
+                {
+                    // start looking for the snake (true ones)
+                    int [] currentPosition = new int [2];
+                    currentPosition[0]= headPosition[0];
+                    currentPosition [1] = headPosition [1];
+                }
+            }
+
+        }
+        // NEIGHBORS AND MAKE SURE THE NEIGHBOR IS NOT THE ONE WE CAME FROM
+        //
+
+    }
+
+    private int[] findTailRecursive (int[] currentPosition, int[] previousPosition)
+    {
+
+    }
     public int neighbors( int row, int col)
     {
         int countSnake = 0;
